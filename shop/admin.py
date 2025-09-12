60722c939b73acfdffb2d7ffc32944cc47e42d67
Anes-py <AnesDevpy@gmail.com>
2025-09-12 16:59:13 +0330

    add product,comment admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Comment
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'time_limit',
+        'traffic_limit',
+        'device_connections_limit',
+        'is_special',
+    ]
+    list_filter = [
+        'is_special',
+    ]
+    search_fields = [
+        'name',
+    ]
+    list_per_page = 10
+    list_max_show_all = 30
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'display_name',
+        'user_city',
+        'text',
+    ]
+    list_per_page = 10
+    list_max_show_all = 30
