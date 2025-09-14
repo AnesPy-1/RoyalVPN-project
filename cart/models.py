9a36f115a4e3ef10416bd2292df30b4e7299d615
Anes-py <AnesDevpy@gmail.com>
2025-09-14 10:44:38 +0330

    add cart&cart items model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from shop.models import Product
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='user_carts',
+        null=True,
+        blank=True,
+    )
+    session_key = models.CharField(max_length=55, blank=True)
+
+    def __str__(self):
+        return f"cart: {self.id}"
+
+    def get_cart_old_total(self):
+        return sum([item.product.price for item in self.items.select_related('product')])
+
+    def get_cart_final_price(self):
+        return sum([item.product.get_final_price() for item in self.items.select_related('product')])
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return f"item: {self.id}"
--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+    path('', include('shop.urls')),
+    path('cart/', include('cart.urls')),
 
 ]
 
