547fa1515b77d85b2a3ab86e51b68ceb68f069d2
Anes-py <AnesDevpy@gmail.com>
2025-09-14 11:07:34 +0330

    add cart detail view

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.db.models import Prefetch
 
+from .models import Cart, CartItem
 
 
 def cart_detail_view(request):
-    return render(request, 'cart/cart.html')
+    if request.user.is_authenticated:
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+    else:
+        if not request.session.session_key:
+            request.session.create()
+        cart, _ = Cart.objects.get_or_create(session_key=request.session.session_key)
+
+    cart = Cart.objects.filter(pk=cart.pk).prefetch_related(
+            Prefetch(
+                'items',
+                queryset=CartItem.objects.select_related('product')
+            )
+        ).select_related('user')
+
+    context = {
+        'cart':cart,
+    }
+    return render(request, 'cart/cart.html', context=context)
