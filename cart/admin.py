a0e0c014fe4e8876479e1ed8448525e0f4672879
Anes-py <AnesDevpy@gmail.com>
2025-09-14 10:51:01 +0330

    add cart admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+    autocomplete_fields = ['product']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+    ]
+
+    inlines = [
+        CartItemInline,
+    ]
