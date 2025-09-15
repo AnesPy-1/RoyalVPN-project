d85a5c514353ae234f75feb68f216d92bd58980b
Anes-py <AnesDevpy@gmail.com>
2025-09-15 13:47:05 +0330

    add payment admin panel

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display =[
+        'order',
+        'user',
+        'total_price',
+        'created_at'
+    ]
+    search_fields = ['user']
+    ordering = [
+        '-created_at'
+    ]
+    list_per_page = 20
+    list_max_show_all = 30
+
